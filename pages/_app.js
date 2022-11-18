26c2ef65caa4de76751df35e20358cfb8a0212cc
luangelsdorf <luanferreira2136@gmail.com>
2022-11-18 09:07:33 -0300

    SmoothScroll hook

--- pages/_app.js
+++ pages/_app.js
@@ -2,27 +2,13 @@ import { useEffect } from 'react';
 import '../styles/globals.css';
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import smoothScroll from '../src/utils/smoothScroll';
 import { useRouter } from 'next/router';
+import smoothScroll from '../src/utils/smoothScroll';
+import useSmoothScroll from '../src/hooks/useSmoothScroll';
 
 function MyApp({ Component, pageProps }) {
 
-  const router = useRouter();
-
-  function initScroll() {
-    ScrollTrigger.killAll();
-    const ss = smoothScroll("#content", undefined, 1.5, gsap, ScrollTrigger);
-    console.log(ss);
-    scrollTo(0, 0);
-  }
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    initScroll();
-
-    router.events.on('routeChangeComplete', initScroll);
-    return () => router.events.off('routeChangeComplete', initScroll);
-  }, []);
+  const scroller = useSmoothScroll();
 
   return (
     <div id="viewport">
